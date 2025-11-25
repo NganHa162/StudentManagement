804522a6a130cbf1e929b4e1fe56a5f75a856783
hungzin311 <hung123ka5@gmail.com>
2025-11-26 00:19:41 +0700

    add StudentDAO

---
+++ src/main/java/org/example/studentmanagement/dao/StudentDAO.java
@@ -0,0 +1,7 @@
+package org.example.studentmanagement.dao;
+
+import org.example.studentmanagement.entity.Student;
+
+public interface StudentDAO extends BaseDAO<Student, Integer> {
+}
+
---
+++ src/main/java/org/example/studentmanagement/dao/StudentDAOImpl.java
@@ -0,0 +1,7 @@
+package org.example.studentmanagement.dao;
+
+import org.example.studentmanagement.entity.Student;
+
+public class StudentDAOImpl extends BaseDAOImpl<Student, Integer> implements StudentDAO{
+}
+

2311195ae436b5666c16fcec3503cc436b7f025c
Junn9042 <khuatan9042@gmail.com>
2025-12-06 21:08:55 +0700

    update TeacherDAO

--- src/main/java/org/example/studentmanagement/dao/TeacherDAO.java
+++ src/main/java/org/example/studentmanagement/dao/TeacherDAO.java
@@ -3,4 +3,8 @@ package org.example.studentmanagement.dao;
 import org.example.studentmanagement.entity.Teacher;
 
 public interface TeacherDAO extends BaseDAO<Teacher, Integer>{
+    // Inherits all CRUD methods from BaseDAO
+    // Can add custom methods here if needed, for example:
+    // Teacher findByUsername(String username);
+    // Teacher findByEmail(String email);
 }

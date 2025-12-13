414bdbb6fda3a7705c383175c7c69ab797d9c69a
thaihung110 <Hung.VT226046@sis.hust.edu.vn>
2025-12-13 18:45:22 +0700

    add code

--- src/main/java/org/example/studentmanagement/repository/AssignmentRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.studentmanagement.repository;
-
-import org.example.studentmanagement.entity.Assignment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AssignmentRepository extends JpaRepository<Assignment, Long> {
-}
-
--- src/main/java/org/example/studentmanagement/repository/CourseRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.studentmanagement.repository;
-
-import org.example.studentmanagement.entity.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepository extends JpaRepository<Course, Long> {
-}
-

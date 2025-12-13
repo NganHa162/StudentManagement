a325ab7faa709b0e9243e6b6b55143c888b47c3d
Junn9042 <khuatan9042@gmail.com>
2025-12-13 11:26:35 +0700

    revert func in StudentService

--- src/main/java/org/example/studentmanagement/service/StudentServiceImpl.java
+++ src/main/java/org/example/studentmanagement/service/StudentServiceImpl.java
@@ -3,14 +3,32 @@ package org.example.studentmanagement.service;
 import org.example.studentmanagement.dao.StudentDAO;
 import org.example.studentmanagement.entity.Student;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentServiceImpl implements StudentService {
 
     private final StudentDAO studentDAO;
+    @Override
+    public Optional<Student> findByUserName(String userName) {
+        return studentDAO.findByUserName(userName);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Student student = findByUserName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Student not found: " + username));
+        return User.withUsername(student.getUserName())
+                .password(student.getPassword())
+                .roles("STUDENT")
+                .build();
+    }
 
     @Autowired
     public StudentServiceImpl(StudentDAO studentDAO) {
